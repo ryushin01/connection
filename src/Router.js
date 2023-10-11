fd2a962a4e850c7bb3a00102414acceee810b781
coolllleooo <buseness200824@gmail.com>
2023-10-11 16:31:55 +0900

    [setting] 초기 세팅

---
+++ src/Router.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Main from './pages/Main/Main';
+import Login from './pages/Login/Login';
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+export default Router;
