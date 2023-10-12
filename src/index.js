a1db45ab25bf9077a7d1259feec987b5e8af29ab
ryushin0 <141554987+ryushin01@users.noreply.github.com>
2023-10-12 20:58:34 +0900

    [fix] 테마 스위처 위치 수정 진행

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
+import { lightTheme, darkTheme } from './styles/theme';
+import ThemeSwitcher from './modules/themeSwitcher';
+import GlobalStyle from './styles/GlobalStyle';
 import Router from './Router';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Router />);
+
+root.render = () => {
+  const [isLightTheme, setIsLightTheme] = useState(true);
+
+  const switchTheme = () => {
+    setIsLightTheme(prev => !prev);
+  };
+
+  return (
+    <ThemeProvider theme={isLightTheme ? lightTheme : darkTheme}>
+      <GlobalStyle />
+      <Router />
+      <ThemeSwitcher switchTheme={switchTheme} isLightTheme={isLightTheme} />
+    </ThemeProvider>
+  );
+};
