f4c229da13844136518d5200fdad3cbbac5434af
ryushin0 <141554987+ryushin01@users.noreply.github.com>
2023-10-12 18:18:34 +0900

    [fix] 테마 스위처 스타일 추가

--- src/modules/themeSwitcher.js
+++ src/modules/themeSwitcher.js
@@ -1,13 +1,37 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const ThemeSwitcher = ({ switchTheme, isLightTheme }) => {
   return (
-    <input
-      type="checkbox"
-      onClick={switchTheme}
-      value={isLightTheme ? 'LIGHT' : 'DARK'}
-    />
+    <ThemeSwitcherWrap>
+      <input
+        type="checkbox"
+        onClick={switchTheme}
+        value={isLightTheme ? 'LIGHT' : 'DARK'}
+      />
+    </ThemeSwitcherWrap>
   );
 };
+
+const ThemeSwitcherWrap = styled.div`
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  z-index: 100;
+  input[type='checkbox'] {
+    appearance: none;
+    width: 40px;
+    height: 40px;
+    background-color: transparent;
+    background-size: cover;
+    background-position: center;
+    &[value='LIGHT'] {
+      background-image: url(/images/ic_dark_theme.png);
+    }
+    &[value='DARK'] {
+      background-image: url(/images/ic_light_theme.png);
+    }
+  }
+`;
 
 export default ThemeSwitcher;
