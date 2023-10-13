6b6d95813236ec772374618c29dc4bb4467697f7
ryushin0 <141554987+ryushin01@users.noreply.github.com>
2023-10-13 10:09:07 +0900

    [fix] 테마 스위치: 멘토팀 피드백 반영

--- src/Root.js
+++ src/Root.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
+import Router from './Router';
 import { ThemeProvider } from 'styled-components';
 import { lightTheme, darkTheme } from './styles/theme';
 import ThemeSwitcher from './modules/themeSwitcher';
 import GlobalStyle from './styles/GlobalStyle';
 
-const App = () => {
+const Root = () => {
   const [isLightTheme, setIsLightTheme] = useState(true);
 
   const switchTheme = () => {
@@ -14,9 +15,10 @@ const App = () => {
   return (
     <ThemeProvider theme={isLightTheme ? lightTheme : darkTheme}>
       <GlobalStyle />
+      <Router />
       <ThemeSwitcher switchTheme={switchTheme} isLightTheme={isLightTheme} />
     </ThemeProvider>
   );
 };
 
-export default App;
+export default Root;
--- src/Router.js
+++ src/Router.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import App from './App';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './pages/Main/Main';
 import Login from './pages/Login/Login';
@@ -7,7 +6,6 @@ import Login from './pages/Login/Login';
 const Router = () => {
   return (
     <BrowserRouter>
-      <App />
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/login" element={<Login />} />
