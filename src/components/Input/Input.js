6fe190c22f437a25782999db15d061d90fc93efb
박요진 <syon013@naver.com>
2023-10-14 16:29:12 +0900

    [fix] 인풋 컴포넌트 / 멘토님 리뷰 수정사항 적용
    1. color는 상황에 맞게 사용되도록 if문으로 분기
    
    2. flex 속성은 Label에서 사용하는 것이기 때문에 명칭을 labelFlex로 props 명 수정
    
    3. custom attribute 삭제
    
    4. focus 시 outLine 은 공통으로 적용되도록 수정
    
    5. 색상은 theme에 있는 컬러 사용되도록 수정

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -2,34 +2,27 @@ import React from 'react';
 import styled from 'styled-components';
 
 function Input({
-  flex = 0,
   type = 'text',
-  shape = 'solid',
-  size = 'medium',
-  color = 'primary',
-  status,
   name,
   value,
   defaultValue,
   onChange,
   disabled,
   placeholder,
+  ...props
 }) {
   return (
     // 인풋 Props
-    <FlexLabel flex={flex}>
+    <FlexLabel {...props}>
       <DefaultInput
         type={type}
-        dataShape={shape}
-        dataSize={size}
-        dataColor={color}
-        dataStatus={status}
         name={name}
         value={value}
         defaultValue={defaultValue}
         onChange={onChange}
         disabled={disabled}
         placeholder={placeholder}
+        {...props}
       />
     </FlexLabel>
   );
@@ -37,12 +30,7 @@ function Input({
 
 /**
  * Input Props 정의
- * @property {number} flex - 인풋 flex 값
  * @property {string} type - 인풋 타입
- * @property {string} dataShape - 인풋 모양 (solid, round)
- * @property {string} dataSize - 인풋 크기 (small, medium)
- * @property {string} dataColor - 인풋 색상 (primary, secondary, neutral)
- * @property {string} dataStatus - 인풋 상태 (default, error, done)
  * @property {string} name - 인풋 이름
  * @property {string} value - 인풋 값 (변경 불가)
  * @property {string} defaultValue - 인풋 기본 값 (변경 가능)
@@ -54,69 +42,53 @@ function Input({
 const INPUT_SIZE = {
   small: {
     height: '40px',
+    fontSize: '16px',
   },
   medium: {
     height: '50px',
+    fontSize: '20px',
   },
   large: {
     height: '60px',
+    fontSize: '24px',
   },
 };
 
 const FlexLabel = styled.label`
   display: flex;
   align-items: center;
-  flex: ${props => props.flex === '1' && '1'};
+  flex: ${({ labelFlex }) => labelFlex || '0'};
 `;
 
 const DefaultInput = styled.input`
   opacity: 0.9;
   width: 100%;
+  height: ${({ size }) => INPUT_SIZE[size]?.height || INPUT_SIZE.medium.height};
   padding: 0 20px;
-  height: ${({ dataSize }) => INPUT_SIZE[dataSize]?.height};
+  outline: none;
+  font-size: ${({ size }) =>
+    INPUT_SIZE[size]?.fontSize || INPUT_SIZE.medium.fontSize};
   border: 1px solid transparent;
-  border-radius: ${props => props.dataShape === 'round' && '50px'};
+  border-radius: ${props => props.borderRadius};
 
-  border-color: ${props =>
-    (props.dataColor === 'primary' && props.theme.primary) ||
-    (props.dataColor === 'secondary' && props.theme.secondary) ||
-    props.theme.grayscaleC};
-
-  background-color: ${props =>
-    (props.dataColor === 'primary' && props.theme.primary) ||
-    (props.dataColor === 'secondary' && props.theme.secondary) ||
-    props.theme.grayscaleB};
-
-  color: ${props =>
-    (props.dataColor === 'primary' && props.theme.primary) ||
-    (props.dataColor === 'secondary' && props.theme.secondary) ||
-    props.theme.grayscaleB};
-
-  &[dataStatus='default'] {
-    border-color: #011627;
-    color: #011627;
-
-    &:focus {
-      outline: none;
+  ${props => {
+    if (props.status === 'error') {
+      return `
+        border-color : ${props.theme.secondaryColor};
+        color : ${props.theme.secondaryColor};
+      `;
+    } else if (props.status === 'done') {
+      return `
+        border-color : ${props.theme.primaryColor};
+        color : ${props.theme.primaryColor};
+      `;
+    } else {
+      return `
+      border-color : ${props.theme.grayscaleC};
+        color : ${props.theme.grayscaleD};
+      `;
     }
-  }
-
-  &[dataStatus='error'] {
-    border-color: #e71d36;
-    color: #e71d36;
-
-    &:focus {
-      outline: none;
-    }
-  }
-
-  &[dataStatus='done'] {
-    border-color: #2ec4b6;
-    color: #2ec4b6;
-
-    &:focus {
-      outline: none;
-    }
-  }
+  }}
 `;
+
 export default Input;
