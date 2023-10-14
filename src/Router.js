011b47e6de3794001a6ea8172a86a7617d7624f4
ryushin0 <141554987+ryushin01@users.noreply.github.com>
2023-10-14 21:22:17 +0900

    [style] 푸터, 판매자 전환 버튼 개발 및 리소스 추가

--- src/Router.js
+++ src/Router.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SellerConversionButton from './components/SellerConversionButton/SellerConversionButton';
+import Footer from './components/Footer/Footer';
 import Main from './pages/Main/Main';
 import Login from './pages/Login/Login';
 
@@ -10,7 +12,10 @@ const Router = () => {
         <Route path="/" element={<Main />} />
         <Route path="/login" element={<Login />} />
       </Routes>
+      <SellerConversionButton />
+      <Footer />
     </BrowserRouter>
   );
 };
+
 export default Router;
