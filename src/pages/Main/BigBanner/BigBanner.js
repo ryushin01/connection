69508e693f5a193f11033ef2c6923c306b4f416d
ryushin0 <141554987+ryushin01@users.noreply.github.com>
2023-10-15 20:48:04 +0900

    [fix] 빅배너 논클릭 이슈 수정

--- src/pages/Main/BigBanner/BigBanner.js
+++ src/pages/Main/BigBanner/BigBanner.js
@@ -59,15 +59,17 @@ const BigBannerWrap = styled.section`
   position: relative;
   font-size: 0;
 
-  &::before {
-    content: '';
-    position: absolute;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    z-index: 10;
-    background-color: rgba(0, 0, 0, 0.2);
+  & > div > div:first-child {
+    &::before {
+      content: '';
+      position: absolute;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      left: 0;
+      z-index: 10;
+      background-color: rgba(0, 0, 0, 0.2);
+    }
   }
 `;
 
