d6a8c9eef52a03806862c755eb8d333e4b6e5e49
박요진 <syon013@naver.com>
2023-10-18 14:25:12 +0900

    [feat][Style] Seller 회원가입 페이지 구현 1차 완료
    1. BackEnd와 통신하여 POST TEST 완료

--- src/pages/SignUp/SellerSignUp.js
+++ src/pages/SignUp/SellerSignUp.js
@@ -3,15 +3,28 @@ import styled from 'styled-components';
 import Input from '../../components/Input/Input';
 import DaumPostCode from './DaumPostCode/DaumPostCode';
 import Button from '../../components/Button/Button';
+import { useNavigate } from 'react-router-dom';
 
 const SellerSignUp = () => {
   const [sellerInfo, setSellerInfo] = useState({
+    // 셀러 회원정보 보관 state
     sellerName: '',
     zipCode: '',
     address: '',
     addressDetail: '',
+    phone: '',
   });
-  const [onAddressSelect, serOnAddressSelect] = useState('');
+  const [onAddressSelect, serOnAddressSelect] = useState(''); // 우편번호 API 실행 후 받아온 data를 state에 저장
+
+  // var
+  const navigate = useNavigate();
+
+  // function
+
+  const handleSellerInfo = e => {
+    const { name, value } = e.target;
+    setSellerInfo({ ...sellerInfo, [name]: value });
+  };
 
   const handleAddressSelect = address => {
     serOnAddressSelect(address);
@@ -22,6 +35,35 @@ const SellerSignUp = () => {
     });
   };
 
+  const postSellerInfoSubmitBtn = e => {
+    e.preventDefault();
+
+    fetch('http://10.58.52.198:8000/users/seller', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+        Authorization: localStorage.getItem('access_token'),
+      },
+      body: JSON.stringify({
+        name: sellerInfo.sellerName,
+        image: 'no-image.jpg',
+        zipCode: sellerInfo.zipCode,
+        address: sellerInfo.address,
+        addressDetails: sellerInfo.addressDetail,
+        phoneNumber: sellerInfo.phone,
+      }),
+    })
+      .then(response => response.json())
+      .then(result => {
+        if (result.message === 'SUCCESS') {
+          alert('판매자 등록이 완료되었습니다.');
+          navigate('/');
+        } else {
+          alert('판매자 등록에 실패하였습니다.');
+        }
+      });
+  };
+
   return (
     <Main id="main">
       <div>
@@ -30,14 +72,17 @@ const SellerSignUp = () => {
             <span>셀러 회원가입</span>
           </SellerSignUpLeftSection>
           <SellerSignUpRightSection>
-            <SellerSignUpForm>
+            <SellerSignUpForm
+              onSubmit={postSellerInfoSubmitBtn}
+              onChange={handleSellerInfo}
+            >
               <fieldset>
                 <SellerSignUpLegend>셀러 회원가입</SellerSignUpLegend>
               </fieldset>
               <SellerSignUpInputWrap>
                 <Input
                   type="text"
-                  placeholder="판매자 닉네임을 입력해주세요."
+                  placeholder="판매자 이름을 입력해주세요."
                   borderRadius="4px"
                   name="sellerName"
                 />
@@ -69,12 +114,21 @@ const SellerSignUp = () => {
                   labelFlex="1"
                 />
               </SellerSignUpInputWrap>
+              <SellerSignUpInputWrap>
+                <Input
+                  type="text"
+                  placeholder="휴대폰 번호"
+                  name="phone"
+                  labelFlex="1"
+                />
+              </SellerSignUpInputWrap>
               <SellerSignUpButtonWrap>
                 <Button
                   type="submit"
                   content="회원가입"
                   shape="solid"
                   color="primary"
+                  onClick={postSellerInfoSubmitBtn}
                 />
               </SellerSignUpButtonWrap>
             </SellerSignUpForm>
