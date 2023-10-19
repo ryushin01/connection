23a1024d405e7bbf41e6dfa7ad946fac7091e57c
ryushin0 <141554987+ryushin01@users.noreply.github.com>
2023-10-19 19:36:18 +0900

    [style] 주문 페이지 UI 개발

--- src/pages/Order/Order.js
+++ src/pages/Order/Order.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 // import { API } from '../../config';
 import Loading from '../Loading/Loading';
 import Button from '../../components/Button/Button';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 
 /**
  * Order.js logics
@@ -12,15 +13,22 @@ import styled, { css } from 'styled-components';
 const Order = () => {
   const [loading, setLoading] = useState(false);
 
+  const navigate = useNavigate();
+
+  const goToBack = () => {
+    navigate(-1);
+  };
+
   return (
     <>
       {loading && <Loading />}
       <main id="main">
         <div>
+          <SectionTitle>주문하기</SectionTitle>
           <Section>
-            <SectionTitle>배송지 정보</SectionTitle>
+            <SectionSubtitle>배송지 정보</SectionSubtitle>
             <TableGroup>
-              <AddressTable>
+              <SectionTable>
                 <caption>보내시는 분</caption>
                 <tbody>
                   <tr>
@@ -36,8 +44,8 @@ const Order = () => {
                     <td>서울시 서대문구 통일로25길 30</td>
                   </tr>
                 </tbody>
-              </AddressTable>
-              <AddressTable>
+              </SectionTable>
+              <SectionTable>
                 <caption>받으시는 분</caption>
                 <tbody>
                   <tr>
@@ -53,176 +61,114 @@ const Order = () => {
                     <td>서울시 서대문구 통일로25길 30</td>
                   </tr>
                 </tbody>
-              </AddressTable>
+              </SectionTable>
             </TableGroup>
           </Section>
+          <Section>
+            <SectionSubtitle>배송 및 결제 방법</SectionSubtitle>
+            <TableGroup>
+              <SectionTable>
+                <caption>배송 방법</caption>
+                <tbody>
+                  <tr>
+                    <th>배송 방법</th>
+                    <td>류창선</td>
+                  </tr>
+                </tbody>
+              </SectionTable>
+              <SectionTable>
+                <caption>결제 방법</caption>
+                <tbody>
+                  <tr>
+                    <th>결제 방법</th>
+                    <td>류창선</td>
+                  </tr>
+                </tbody>
+              </SectionTable>
+            </TableGroup>
+          </Section>
+          <ButtonGroup>
+            <Button
+              shape="solid"
+              color="neutral"
+              size="large"
+              content="돌아가기"
+              onClick={goToBack}
+            />
+            <Button
+              shape="solid"
+              color="primary"
+              size="large"
+              content="결제하기"
+            />
+          </ButtonGroup>
         </div>
       </main>
     </>
   );
 };
 
-const FlexCenter = css`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Section = styled.section`
-  position: relative;
-  color: ${props => props.theme.grayscaleF};
-
-  table {
-    table-layout: fixed;
-    width: 100%;
-    flex: 1;
-    margin-top: 20px;
-
-    caption {
-      font-size: 32px;
-      text-align: left;
-    }
-
-    th,
-    td {
-      font-size: 24px;
-      line-height: 1.5;
-      text-align: left;
-    }
-
-    th {
-      width: 10%;
-    }
-  }
-`;
-
 const SectionTitle = styled.h2`
   font-size: 32px;
   text-align: center;
 `;
 
-const TableGroup = styled.div`
-  display: flex;
+const Section = styled.section`
+  position: relative;
+  padding: 80px 0;
+  color: ${props => props.theme.grayscaleF};
+
+  & + section {
+    border-top: 1px ${props => props.theme.grayscaleD} solid;
+  }
+
+  table {
+    table-layout: fixed;
+    width: 100%;
+    flex: 1;
+
+    caption {
+      padding: 12px 0;
+      border-bottom: 1px ${props => props.theme.grayscaleF} solid;
+      font-size: 24px;
+      text-align: left;
+    }
+
+    th,
+    td {
+      padding: 12px 4px;
+      font-size: 16px;
+      line-height: 1.5;
+      text-align: left;
+    }
+
+    th {
+      width: 15%;
+    }
+  }
 `;
 
-const AddressTable = styled.table`
+const SectionSubtitle = styled.h3`
+  margin-bottom: 40px;
+  font-size: 28px;
+`;
+
+const TableGroup = styled.div`
+  display: flex;
+  gap: 8vw;
+`;
+
+const SectionTable = styled.table`
   position: relative;
 `;
 
-// const DetailTopSection = styled.section`
-//   position: relative;
-//   display: flex;
-//   border-bottom: 1px ${props => props.theme.grayscaleC} solid;
-
-//   & > div {
-//     flex: 1;
-//     padding-top: 7vw;
-//     padding-bottom: 7vw;
-//   }
-// `;
-
-// const ImageArea = styled.div`
-//   ${FlexCenter};
-// `;
-
-// const ImageAreaInnerWrap = styled.div`
-//   ${FlexCenter};
-//   position: relative;
-//   width: 30vw;
-//   height: 30vw;
-//   border-radius: 4px;
-//   background-color: ${props => props.theme.grayscaleB};
-
-//   img {
-//     width: 20vw;
-//     height: 20vw;
-//     object-fit: contain;
-//   }
-// `;
-
-// const MetadataArea = styled.div`
-//   padding-left: 3vw;
-//   padding-right: 3vw;
-//   font-size: 24px;
-// `;
-
-// const ProductTitle = styled.h2`
-//   display: -webkit-box;
-//   overflow: hidden;
-//   text-overflow: ellipsis;
-//   -webkit-line-clamp: 2;
-//   -webkit-box-orient: vertical;
-//   word-wrap: break-word;
-//   white-space: normal;
-//   font-size: 32px;
-//   line-height: 1.2;
-// `;
-
-// const MetadataAreaInnerWrap = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   justify-content: space-between;
-//   position: relative;
-//   height: 100%;
-// `;
-
-// const MetadataTableWrap = styled.div`
-//   position: relative;
-// `;
-
-// const MetadataTable = styled.table`
-//   table-layout: fixed;
-//   width: 100%;
-//   text-align: left;
-
-//   th,
-//   td {
-//     line-height: 1.4;
-//   }
-
-//   th {
-//     width: 35%;
-//   }
-
-//   tr:nth-child(2) {
-//     td {
-//       color: ${props => props.theme.secondaryColor};
-//     }
-//   }
-
-//   tr:last-child {
-//     td {
-//       color: ${props => props.theme.primaryColor};
-//     }
-//   }
-// `;
-
-// const PriceDisplay = styled.div`
-//   display: flex;
-//   width: 100%;
-//   justify-content: center;
-
-//   & > div {
-//     flex: 1;
-//   }
-
-//   & > span {
-//     flex: 2;
-//     display: flex;
-//     justify-content: flex-end;
-//     align-items: center;
-//     font-size: 32px;
-//   }
-// `;
-
-// const ButtonGroup = styled.div`
-//   display: flex;
-//   gap: 16px;
-//   width: 100%;
-// `;
-
-// const DetailBottomSection = styled.section`
-//   position: relative;
-// `;
+const ButtonGroup = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 50%;
+  margin: 0 auto 40px;
+  gap: 4vw;
+`;
 
 export default Order;
