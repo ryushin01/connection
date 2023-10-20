510b6dbce90744439d653d222c9db26705aa7118
ryushin0 <141554987+ryushin01@users.noreply.github.com>
2023-10-21 00:59:59 +0900

    [style][fix] 결제 완료 페이지 개발 및 글로벌 검색 스펙 아웃 처리

--- src/pages/Payment/Payment.js
+++ src/pages/Payment/Payment.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 // import { API } from '../../config';
+import { ReactComponent as CheckIcon } from '../../svg/icon_check.svg';
 import Loading from '../Loading/Loading';
 import Button from '../../components/Button/Button';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 /**
  * Order.js logics
@@ -12,100 +13,162 @@ import styled from 'styled-components';
 
 const Payment = () => {
   const [loading, setLoading] = useState(false);
+  const [paymentComplete, setPaymentComplete] = useState(false);
   const navigate = useNavigate();
+
+  const advancedPaymentComplete = () => {
+    setPaymentComplete(true);
+  };
 
   return (
     <>
       {loading && <Loading />}
-      <main id="main">
+      <Main id="main">
         <div>
-          <SectionTitle>결제하기</SectionTitle>
-          <Section>
-            <SectionSubtitle>주문 정보</SectionSubtitle>
-            <TableGroup>
-              <SectionTable>
-                <colgroup>
-                  <col style={{ width: '50%' }} />
-                  <col style={{ width: '20%' }} />
-                  <col style={{ width: '30%' }} />
-                </colgroup>
-                <caption>주문 정보</caption>
-                <thead>
-                  <tr>
-                    <th>제품명</th>
-                    <td>수량</td>
-                    <td>가격</td>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <th>밀키트</th>
-                    <td>1</td>
-                    <td>10,000</td>
-                  </tr>
-                  <tr>
-                    <th>밀키트</th>
-                    <td>1</td>
-                    <td>10,000</td>
-                  </tr>
-                </tbody>
-              </SectionTable>
-              <SectionTable>
-                <colgroup>
-                  <col style={{ width: '20%' }} />
-                  <col style={{ width: '80%' }} />
-                </colgroup>
-                <caption>결제 금액</caption>
-                <tbody>
-                  <tr>
-                    <th>상품 금액</th>
-                    <td>-</td>
-                  </tr>
-                  <tr>
-                    <th>할인 금액</th>
-                    <td>-</td>
-                  </tr>
-                  <tr>
-                    <th>배송비</th>
-                    <td>-</td>
-                  </tr>
-                  <tr>
-                    <th>총 결제 금액</th>
-                    <td>-</td>
-                  </tr>
-                  <tr>
-                    <th>포인트 차감</th>
-                    <td>
-                      <span>5,000</span>
-                      <RemainingPoints>
-                        (잔여 포인트: <strong>10,000</strong>)
-                      </RemainingPoints>
-                    </td>
-                  </tr>
-                </tbody>
-              </SectionTable>
-            </TableGroup>
-          </Section>
-          <ButtonGroup>
-            <Button
-              shape="solid"
-              color="neutral"
-              size="large"
-              content="돌아가기"
-              onClick={() => navigate(-1)}
-            />
-            <Button
-              shape="solid"
-              color="primary"
-              size="large"
-              content="결제하기"
-            />
-          </ButtonGroup>
+          {!paymentComplete && (
+            <Before>
+              <SectionTitle>결제하기</SectionTitle>
+              <Section>
+                <SectionSubtitle>주문 정보</SectionSubtitle>
+                <TableGroup>
+                  <SectionTable>
+                    <colgroup>
+                      <col style={{ width: '50%' }} />
+                      <col style={{ width: '20%' }} />
+                      <col style={{ width: '30%' }} />
+                    </colgroup>
+                    <caption>주문 정보</caption>
+                    <thead>
+                      <tr>
+                        <th>제품명</th>
+                        <td>수량</td>
+                        <td>가격</td>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <th>밀키트</th>
+                        <td>1</td>
+                        <td>10,000</td>
+                      </tr>
+                      <tr>
+                        <th>밀키트</th>
+                        <td>1</td>
+                        <td>10,000</td>
+                      </tr>
+                    </tbody>
+                  </SectionTable>
+                  <SectionTable>
+                    <colgroup>
+                      <col style={{ width: '20%' }} />
+                      <col style={{ width: '80%' }} />
+                    </colgroup>
+                    <caption>결제 금액</caption>
+                    <tbody>
+                      <tr>
+                        <th>상품 금액</th>
+                        <td>-</td>
+                      </tr>
+                      <tr>
+                        <th>할인 금액</th>
+                        <td>-</td>
+                      </tr>
+                      <tr>
+                        <th>배송비</th>
+                        <td>-</td>
+                      </tr>
+                      <tr>
+                        <th>총 결제 금액</th>
+                        <td>-</td>
+                      </tr>
+                      <tr>
+                        <th>포인트 차감</th>
+                        <td>
+                          <span>5,000</span>
+                          <RemainingPoints>
+                            (잔여 포인트: <strong>10,000</strong>)
+                          </RemainingPoints>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </SectionTable>
+                </TableGroup>
+              </Section>
+              <ButtonGroup>
+                <Button
+                  shape="solid"
+                  color="neutral"
+                  size="large"
+                  content="돌아가기"
+                  onClick={() => navigate(-1)}
+                />
+                <Button
+                  shape="solid"
+                  color="primary"
+                  size="large"
+                  content="결제하기"
+                  onClick={advancedPaymentComplete}
+                />
+              </ButtonGroup>
+            </Before>
+          )}
+
+          {paymentComplete && (
+            <After>
+              <SectionTitle>결제 완료</SectionTitle>
+              <Section>
+                <SectionInnerWrap>
+                  <CheckIcon />
+                  <OrderNumber>주문 번호: 100-2023-1021</OrderNumber>
+                </SectionInnerWrap>
+              </Section>
+              <ButtonGroup>
+                <Button
+                  shape="solid"
+                  color="neutral"
+                  size="large"
+                  content="주문 내역 확인하러 가기"
+                  onClick={() => navigate('/mypage')}
+                />
+                <Button
+                  shape="solid"
+                  color="primary"
+                  size="large"
+                  content="메인 화면으로 돌아가기"
+                  onClick={() => navigate('/')}
+                />
+              </ButtonGroup>
+            </After>
+          )}
         </div>
-      </main>
+      </Main>
     </>
   );
 };
+
+const FlexCenter = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Main = styled.main`
+  display: flex;
+  align-items: center;
+`;
+
+const Before = styled.div`
+  position: relative;
+`;
+
+const After = styled.div`
+  ${FlexCenter};
+  flex-direction: column;
+
+  section {
+    flex: 1;
+  }
+`;
 
 const SectionTitle = styled.h2`
   font-size: 32px;
@@ -149,6 +212,25 @@ const Section = styled.section`
       }
     }
   }
+`;
+
+const SectionInnerWrap = styled.div`
+  ${FlexCenter};
+  flex-direction: column;
+  gap: 4vw;
+
+  svg {
+    fill: ${props => props.theme.grayscaleF};
+    path {
+      fill: ${props => props.theme.grayscaleF};
+      stroke: ${props => props.theme.grayscaleF};
+    }
+  }
+`;
+
+const OrderNumber = styled.em`
+  color: ${props => props.theme.grayscaleF};
+  font-size: 24px;
 `;
 
 const SectionSubtitle = styled.h3`
