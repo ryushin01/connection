e5c90b89d09f716afcb82e8d88823d20633ac21c
박요진 <syon013@naver.com>
2023-10-24 16:09:27 +0900

    [fix] [style] CartSelect 박스  UI 1차 수정
    1. 처음에 가져오는 데이터 표시를 위해 option 태그를 별개로 하나 빼고 중복된 수량일 수 있기에 별개 표시

--- src/components/CartCount/CartCount.js
+++ src/components/CartCount/CartCount.js
@@ -3,18 +3,19 @@ import styled from 'styled-components';
 
 function CartCount({ onQuantityChange, productId, quantity }) {
   // 부모 컴포넌트에서 받아온 props
-  const options = [1, 2, 3, 4, 5, 6, 7, 8, 9, '10+']; // 옵션 value 값을 map으로 돌리기 위한 배열 변수
+  const selectQuantity = [1, 2, 3, 4, 5, 6, 7, 8, 9, '10+ (직접 입력)'];
+
   return (
     <SelectCounterWrap>
       <select
         onChange={e => onQuantityChange(productId, e.target.value)} // 부모 컴포넌트에서 받아온 함수에 productId와 선택한 옵션 value 값을 넘겨줌
         value={quantity} // 서버에서 받아온 quantity 값으로 초기화
       >
-        {options.map((option, index) => {
-          // 옵션 value 값으로 map 돌리기
+        <option value={quantity}>{quantity} (select)</option>
+        {selectQuantity.map((item, index) => {
           return (
-            <option value={option} key={index}>
-              {option}
+            <option key={index} value={item}>
+              {item}
             </option>
           );
         })}
