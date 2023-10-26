2ed93ff6e4d0a70e732120f90163565f415d26bf
ryushin0 <141554987+ryushin01@users.noreply.github.com>
2023-10-26 12:09:45 +0900

    [fix] AWS config 수정

--- src/config.js
+++ src/config.js
@@ -1,10 +1,8 @@
 // AWS
-// const BASE_URL = 'http://43.200.31.13:8000';
+const BASE_URL = 'http://13.209.118.105:8000';
 
-const BASE_URL = 'http://10.58.52.156:8000';
-
+// const BASE_URL = 'http://10.58.52.156:8000';
 // const BASE_URL = 'http://10.58.52.138:8000';
-
 // const BASE_URL = 'http://10.58.52.202:8000';
 
 export const API = {
